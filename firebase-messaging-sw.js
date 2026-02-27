8e4673ec3b1c795d1d657f5e45ef24d027e930a4
villamanzanares <jjgg2007@gmail.com>
2026-02-27 17:32:07 -0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.3.1/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDzKHOwWJIuC4_f2OMuoEyMxJnucC-jr5I",
+  authDomain: "alerta-rosko.firebaseapp.com",
+  projectId: "alerta-rosko",
+  storageBucket: "alerta-rosko.firebasestorage.app",
+  messagingSenderId: "1022811358317",
+  appId: "1:1022811358317:web:ce210848e7ed63d1412b64"
+});
+
+// Inicializar Messaging
+const messaging = firebase.messaging();
